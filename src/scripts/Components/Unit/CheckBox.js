f32500b03a2d7841bf6bfec92be2840d4f9d9cea
Antonis Karamitros @antouank <ablackjim228@gmail.com>
2016-12-06 06:58:38 +0000

    CheckBox can now be disabled - style the checkbox component based on the props given - remove lint warning

--- src/scripts/Components/Unit/CheckBox.js
+++ src/scripts/Components/Unit/CheckBox.js
@@ -1,7 +1,8 @@
 
 'use strict';
 
-const React = require('react');
+const React   = require('react');
+const assign  = require('object-assign');
 
 
 //  -------------------------------- styles
@@ -11,6 +12,7 @@ const StyleLabel =
   , userSelect: 'none'
   , WebkitUserSelect: 'none'
   , cursor: 'pointer'
+  , transition: 'all 0.4s ease-in-out'
   };
 //  --------------------------------
 
@@ -18,6 +20,7 @@ const StyleLabel =
 const CheckBox = React.createClass({
   propTypes:
     { checked: React.PropTypes.bool
+    , disabled: React.PropTypes.bool
     , label: React.PropTypes.string
     , onChange: React.PropTypes.func
     }
@@ -25,14 +28,24 @@ const CheckBox = React.createClass({
 
   render() {
     let checked = !!this.props.checked;
+    let disabled = !!this.props.disabled;
+    //  make the style depending on the props
+    let styleLabel =
+      assign
+        ( {}
+        , StyleLabel
+        , checked ? { textDecoration: 'underline' } : null
+        , { opacity: disabled ? 0.4 : 1 }
+        );
 
-    return (
+    return ( 
       <label>
-        <span style={StyleLabel}>
+        <span style={styleLabel}>
           {this.props.label}
         </span>
         <input
           checked={checked}
+          disabled={disabled}
           onChange={this.props.onChange}
           type='checkbox'
         />
